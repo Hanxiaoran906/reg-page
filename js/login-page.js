558fe3fda36b939c97da635258066f40dde4dbe6
HanPeng <582114650@qq.com>
2018-09-29 19:24:23 +0800

    asdf

--- js/login-page.js
+++ js/login-page.js
@@ -1,3 +1,21 @@
+var countdown=60;
+function settime(obj) {
+    if (countdown == 0) {
+        obj.removeAttribute("disabled");
+        obj.value="免费获取验证码";
+        countdown = 60;
+        return;
+    } else {
+        obj.setAttribute("disabled", true);
+        obj.value="重新发送(" + countdown + ")";
+        countdown--;
+    }
+    setTimeout(function() {
+            settime(obj) }
+        ,1000)
+}
+
+//rem随屏幕变化
 function adapt() {
     var winWidth = $(window).width();
     var fontSize = (winWidth / 1440) * 100;
@@ -10,23 +28,26 @@ function adapt() {
     }else {
         $("html").css("font-size", otherSize + "px")
     }
-
-
 }
+//监测屏幕宽度变化
 $(window).resize(function () {
     adapt()
 });
 setTimeout(adapt,200);
 
+//表单元素聚焦
 function focus () {
     $(this).prev("div").css("color","#29b078");
 }
+//表单元素失焦
 function blur () {
     $(this).prev("div").css("color","#25272e")
 }
+//立即领取按钮
 function obtain () {
     $(".shade").css("display","block");
 }
+//遮罩关闭注册
 function cancel () {
     $(".shade").css("display","none");
 }
