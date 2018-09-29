06ed44d8480c84b2659aa8dcecfbe09762798809
HanPeng <582114650@qq.com>
2018-09-29 15:41:46 +0800

    111

--- js/login-page.js
+++ js/login-page.js
@@ -1,12 +1,26 @@
-$("#obtain").click(function () {
-    $(".shade").css("display","block");
+function adapt() {
+    var winWidth = $(window).width();
+    var fontSize = (winWidth / 1440) * 100;
+    // console.log($("html").css("font-size"));
+    $("html").css("font-size",(fontSize < 100 ? fontSize : 100) + "px");
+}
+$(window).resize(function () {
+    adapt()
 });
-$(".shade-background").click(function () {
-    $(".shade").css("display","none");
-});
-$(".reg-text").on("focus",function () {
-    $(this).prev("div").css("color","#29b078")
-});
-$(".reg-text").on("blur",function () {
+setTimeout(adapt,200);
+
+function focus () {
+    $(this).prev("div").css("color","#29b078");
+}
+function blur () {
     $(this).prev("div").css("color","#25272e")
-});
+}
+function obtain () {
+    $(".shade").css("display","block");
+}
+function cancel () {
+    $(".shade").css("display","none");
+}
+$(".reg-text").on("focus",focus);
+$(".reg-text").on("blur",blur);
+
